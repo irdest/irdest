3ea65b74425d7673bdfa8e4a07b18c3e8e2d4432
Katharina Fey <kookie@spacekookie.de>
2020-10-07 14:40:51 +0200

    qrpc-sdk: implemeting server listen hook

--- clients/hubd/src/main.rs
+++ clients/hubd/src/main.rs
@@ -7,9 +7,9 @@ mod log;
 mod state;
 mod upnp;
 
-use std::time::Duration;
-use async_std::{task, future, task::Poll};
+use async_std::{future, task, task::Poll};
 use state::State;
+use std::time::Duration;
 
 use tracing::error;
 
@@ -18,26 +18,24 @@ pub(crate) fn elog<S: Into<String>>(msg: S, code: u16) -> ! {
     std::process::exit(code.into());
 }
 
+use qrpc_sdk::RpcSocket;
+
 #[async_std::main]
 async fn main() {
     log::parse_log_level();
 
-    let app = cfg::cli();
-    let cfg = cfg::match_fold(app);
-    let _state = State::new(&cfg).await;
+    // let app = cfg::cli();
+    // let cfg = cfg::match_fold(app);
+    // let _state = State::new(&cfg).await;
 
-    // !no_upnp means upnp has _not_ been disabled
-    if !cfg.no_upnp {
-        let cfg = cfg.clone();
-        // the upnp crate does synchronous i/o and we don't want to block this async worker
-        std::thread::spawn(move || {
-            if upnp::open_port(cfg.port).is_none() {
-                error!("Failed to open UPNP port; your router probably doesn't support it...");
-            }
-        });
-    }
+    // // !no_upnp means upnp has _not_ been disabled
+    // if !cfg.no_upnp {
+    //     if upnp::open_port(cfg.port).is_none() {
+    //         error!("Failed to open UPNP port; your router probably doesn't support it...");
+    //     }
+    // }
 
-    let _ = future::timeout(Duration::from_secs(10), async {
-        let _: () = future::poll_fn(|_| Poll::Pending).await;
-    }).await;
+    // let _ = future::timeout(Duration::from_secs(10), async {
+    //     let _: () = future::poll_fn(|_| Poll::Pending).await;
+    // }).await;
 }
