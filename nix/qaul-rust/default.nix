9298c4cb7b23e553c06d43e7ba0a658f891f8616
Milan Pässler <milan@petabyte.dev>
2021-02-26 14:39:45 +0100

    Add Rust unit tests CI

--- nix/qaul-rust/default.nix
+++ nix/qaul-rust/default.nix
@@ -8,8 +8,20 @@
 , buildPackages
 }:
 
-naersk.buildPackage {
-  src = lib.cleanSource ../../.;
+naersk.buildPackage rec {
+  src = lib.cleanSourceWith {
+    filter = lib.cleanSourceFilter;
+    src = lib.cleanSourceWith {
+      filter = name: type:
+        !(lib.hasPrefix "${toString ../../.}/emberweb" name) &&
+        !(lib.hasPrefix "${toString ../../.}/docs" name) &&
+        !(lib.hasPrefix "${toString ../../.}/target" name) &&
+        !(lib.hasPrefix "${toString ../../.}/nix" name)
+      ;
+      src = ../../.;
+    };
+  };
+
   nativeBuildInputs = [
     capnproto
     cmake
@@ -21,4 +33,18 @@ naersk.buildPackage {
   ];
 
   SODIUM_USE_PKG_CONFIG = 1;
+
+  passthru.testBinaries = naersk.buildPackage {
+    inherit src nativeBuildInputs buildInputs SODIUM_USE_PKG_CONFIG;
+
+    cargoBuildOptions = (x: x ++ [ "--tests" ]);
+
+    release = false;
+
+    installPhase = ''
+      find target/debug/deps -type f -executable -regex ".*-[0-9a-f]+" \
+        | sed 's#\(.*\)/\([^/]*\)-\([0-9a-f]\+\)#install -Dm755 \1/\2-\3 \$out/bin/\2-test#' \
+        | sh
+    '';
+  };
 }
