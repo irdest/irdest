02c3b5aea3aa1d27afbf288929aa791deb1f2319
Katharina Fey <kookie@spacekookie.de>
2021-02-28 18:49:39 +0100

    libqaul: implement rpc user API

--- libqaul/src/rpc.rs
+++ libqaul/src/rpc.rs
@@ -9,7 +9,9 @@
 //! (libqaul-rpc) for type and API configuration.
 
 use crate::{
+    error::Error,
     types::rpc::{Capabilities, Reply, UserCapabilities, UserReply},
+    users::{UserAuth, UserProfile, UserUpdate},
     Identity, QaulRef,
 };
 use async_std::{sync::Arc, task};
@@ -77,21 +79,95 @@ impl RpcServer {
     }
 
     async fn spawn_on_request(self: &Arc<Self>, msg: Message, cap: Capabilities) {
+        use Capabilities::*;
+        use UserCapabilities as UserCap;
+
         let reply = match cap {
-            Capabilities::Users(UserCapabilities::Create { pw }) => self
-                .inner
-                .users()
-                .create(pw.as_str())
-                .await
-                .map(|auth| Reply::Users(UserReply::Auth(auth)))
-                .map_err(|e| Reply::Error(e)),
+            // =^-^= User API functions =^-^=
+            Users(UserCap::List) => self.user_list().await,
+            Users(UserCap::ListRemote) => self.user_list_remote().await,
+            Users(UserCap::IsAuthenticated { auth }) => self.user_is_authenticated(auth).await,
+            Users(UserCap::Create { pw }) => self.user_create(pw.as_str()).await,
+            Users(UserCap::Delete { auth }) => self.user_delete(auth).await,
+            Users(UserCap::ChangePw { auth, new_pw }) => self.user_change_pw(auth, new_pw),
+            Users(UserCap::Login { id, pw }) => self.user_login(id, pw).await,
+            Users(UserCap::Logout { auth }) => self.user_logout(auth).await,
+            Users(UserCap::Get { id }) => self.user_get(id).await,
+            Users(UserCap::Update { auth, update }) => self.user_update(auth, update).await,
+
+            // =^-^= Everything else is todo! =^-^=
             _ => todo!(),
-        }
-        .unwrap();
+        };
 
         self.socket
             .reply(msg.reply("...".into(), reply.to_json().as_bytes().to_vec()))
             .await
             .unwrap();
+    }
+
+    /////// Internal command wrappers
+
+    async fn user_list(self: &Arc<Self>) -> Reply {
+        Reply::Users(UserReply::List(self.inner.users().list().await))
+    }
+
+    async fn user_list_remote(self: &Arc<Self>) -> Reply {
+        Reply::Users(UserReply::List(self.inner.users().list().await))
+    }
+
+    async fn user_is_authenticated(self: &Arc<Self>, auth: UserAuth) -> Reply {
+        match self.inner.users().is_authenticated(auth).await {
+            Ok(()) => Reply::Users(UserReply::Ok),
+            Err(e) => Reply::Error(e),
+        }
+    }
+
+    async fn user_create(self: &Arc<Self>, pw: &str) -> Reply {
+        match self.inner.users().create(pw).await {
+            Ok(auth) => Reply::Users(UserReply::Auth(auth)),
+            Err(e) => Reply::Error(e),
+        }
+    }
+
+    async fn user_delete(self: &Arc<Self>, auth: UserAuth) -> Reply {
+        match self.inner.users().delete(auth).await {
+            Ok(()) => Reply::Users(UserReply::Ok),
+            Err(e) => Reply::Error(e),
+        }
+    }
+
+    fn user_change_pw(self: &Arc<Self>, auth: UserAuth, pw: String) -> Reply {
+        match self.inner.users().change_pw(auth, pw.as_str()) {
+            Ok(()) => Reply::Users(UserReply::Ok),
+            Err(e) => Reply::Error(e),
+        }
+    }
+
+    async fn user_login(self: &Arc<Self>, id: Identity, pw: String) -> Reply {
+        match self.inner.users().login(id, pw.as_str()).await {
+            Ok(auth) => Reply::Users(UserReply::Auth(auth)),
+            Err(e) => Reply::Error(e),
+        }
+    }
+
+    async fn user_logout(self: &Arc<Self>, auth: UserAuth) -> Reply {
+        match self.inner.users().logout(auth).await {
+            Ok(()) => Reply::Users(UserReply::Ok),
+            Err(e) => Reply::Error(e),
+        }
+    }
+
+    async fn user_get(self: &Arc<Self>, id: Identity) -> Reply {
+        match self.inner.users().get(id).await {
+            Ok(profile) => Reply::Users(UserReply::Profile(profile)),
+            Err(e) => Reply::Error(e),
+        }
+    }
+
+    async fn user_update(self: &Arc<Self>, auth: UserAuth, update: UserUpdate) -> Reply {
+        match self.inner.users().update(auth, update).await {
+            Ok(()) => Reply::Users(UserReply::Ok),
+            Err(e) => Reply::Error(e),
+        }
     }
 }
