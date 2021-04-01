db315c8f931a5f68aff27498780e1b71185f17e4
Katharina Fey <kookie@spacekookie.de>
2021-04-01 18:32:39 +0200

    Remove legacy ember ui

--- emberweb/app/adapters/user.js
+++
@@ -1,9 +0,0 @@
-import ApplicationAdapter from './application';
-
-export default class UserAdapter extends ApplicationAdapter {
-  urlForQuery(query) {
-    return {
-      contact: () => '/http/contact',
-    }[query.type]();
-  }
-}
