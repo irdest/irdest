e44ddca6472d8f2fb2e2b8c89dbfbfed72698a49
Katharina Fey <kookie@spacekookie.de>
2021-05-11 20:57:10 +0200

    webui: fix initialisation and basic test

--- services/webui/src/lib.rs
+++ services/webui/src/lib.rs
@@ -1,24 +1,31 @@
 //! Irdest webui service library
 
-use irpc_sdk::RpcSocket;
+use irpc_sdk::{Capabilities, RpcSocket, Service};
+use std::sync::Arc;
 use tide::Server;
 
 pub struct WebServer {
     inner: Server<()>,
-    rpc: RpcSocket,
+    rpc: Arc<RpcSocket>,
 }
 
 impl WebServer {
-    pub fn new(rpc: RpcSocket) -> Self {
+    pub async fn new(addr: &str, port: u16) -> Self {
         let mut inner = Server::new();
 
+        let mut serv = Service::new("org.irdest.webui", 1, "Webui.");
+        let rpc = RpcSocket::connect(addr, port).await.unwrap();
+        serv.register(&rpc, Capabilities::basic_json())
+            .await
+            .unwrap();
+
         // Add routes here
-        inner.at("/").get(|_| Ok("Hello world"));
+        inner.at("/").get(|_| async { Ok("Hello world") });
 
         Self { inner, rpc }
     }
 
     pub async fn listen(self, bind: &str) {
-        self.inner.listen(bind).await;
+        self.inner.listen(bind).await.unwrap();
     }
 }
