b5dcf997a8f4c7fad615b79aa9b6015664926d89
Katharina Fey <kookie@spacekookie.de>
2021-05-11 20:38:00 +0200

    webui: init basic tide server

--- services/webui/src/lib.rs
+++ services/webui/src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//! Irdest webui service library
+
+use irpc_sdk::RpcSocket;
+use tide::Server;
+
+pub struct WebServer {
+    inner: Server<()>,
+    rpc: RpcSocket,
+}
+
+impl WebServer {
+    pub fn new(rpc: RpcSocket) -> Self {
+        let mut inner = Server::new();
+
+        // Add routes here
+        inner.at("/").get(|_| Ok("Hello world"));
+
+        Self { inner, rpc }
+    }
+
+    pub async fn listen(self, bind: &str) {
+        self.inner.listen(bind).await;
     }
 }
