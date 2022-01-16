c2d769f042a46a396f118b9d18d99572793bfe63
Katharina Fey <kookie@spacekookie.de>
2022-01-17 00:15:43 +0100

    ratman-client: bump version to be identical to ratman release

--- ratman/client-lib/src/lib.rs
+++ ratman/client-lib/src/lib.rs
@@ -7,6 +7,25 @@
 //! These data formats are outlined in [ratman-types](ratman_types)!
 //!
 //! This crate provides a simple API over these API messages!
+//!
+//! **This API is very unstable!** You may expect _slightly_ more
+//! stability guarantees from `ratcat(1)` and `ratctl(1)`
+//!
+//! ## Version numbers
+//!
+//! The client library MAJOR and MINOR version follow a particular
+//! Ratman release.  So for example, version `0.3.0` of this crate is
+//! built against version `0.3.0` of `ratmand`.  Because Ratman itself
+//! follows semantic versioning, this crate is in turn also
+//! semantically versioned.
+//!
+//! Any change that needs to be applied to this library that does not
+//! impact `ratmand` or the stability of this API will be implemented
+//! as a patch-version.
+//!
+//! Also: by default this library will refuse to connect to a running
+//! `ratmand` that does not match the libraries version number.  This
+//! behaviour can be disabled via the `RatmanIpc` API.
 
 #[macro_use]
 extern crate tracing;
