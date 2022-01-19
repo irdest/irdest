90577f6c86eaeb90d8aaf0d4fe5df8f2c6464d53
Katharina Fey <kookie@spacekookie.de>
2022-01-19 21:57:38 +0100

    irdest-proxy: init project structure

---
+++ clients/irdest-proxy/src/main.rs
@@ -0,0 +1,17 @@
+//! Irdest Proxy allows you to send IP data through an Irdest network
+//!
+//! It uses (at some point in the future) a few different mechanisms
+//! to achieve this:
+//!
+//! - Simple socket + port mapped to a Ratman address
+//!
+//! - Tap device mapping a whole IP space to a Ratman address
+//!
+//! - Tap device mapping different IP spaces to different Ratman
+//!   addresses, using dynamic route announcements (a la BGP).
+//!
+//! Currently ___0___ of these mechanisms are implemented :)
+
+fn main() {
+    todo!()
+}
