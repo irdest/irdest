7823bc16ea5c167f4010dc614fe141bec3785643
Katharina Fey <kookie@spacekookie.de>
2022-02-15 20:21:18 +0100

    netmod-inet: add basic integration tests

--- netmods/netmod-inet/src/lib.rs
+++ netmods/netmod-inet/src/lib.rs
@@ -75,7 +75,7 @@ pub struct Endpoint {
 impl Endpoint {
     /// Create a new endpoint on an interface and port
     #[tracing::instrument(level = "info")]
-    pub async fn new(bind: &str, name: &str, mode: Mode) -> Result<Arc<Self>> {
+    pub async fn new(bind: &str, _: &str, mode: Mode) -> Result<Arc<Self>> {
         info!("Initialising Tcp backend");
 
         let pessimistic = Arc::new(false.into());
@@ -177,5 +177,65 @@ impl EndpointExt for Endpoint {
 
     async fn next(&self) -> netmod::Result<(Frame, Target)> {
         Ok(self.server.next().await)
+    }
+}
+
+#[cfg(test)]
+async fn setup_eps(pa: u16, pb: u16) -> (Arc<Endpoint>, Arc<Endpoint>) {
+    let ep1 = Endpoint::new(&format!("127.0.0.1:{}", pa), "ratmand", Mode::Dynamic)
+        .await
+        .unwrap();
+    let ep2 = Endpoint::new(&format!("127.0.0.1:{}", pb), "ratmand", Mode::Dynamic)
+        .await
+        .unwrap();
+
+    ep1.add_peers(vec![format!("127.0.0.1:{}", pb)])
+        .await
+        .unwrap();
+    ep2.add_peers(vec![format!("127.0.0.1:{}", pa)])
+        .await
+        .unwrap();
+
+    (ep1, ep2)
+}
+
+/// This test creates two endpoints and sends messages through them to
+/// see if a simple handshake can successfully occur.
+#[async_std::test]
+async fn basic_integration() {
+    let (ep1, ep2) = setup_eps(7000, 7001).await;
+
+    let f1 = Frame::dummy();
+    ep1.send(f1.clone(), Target::Single(0)).await.unwrap();
+    let (f2, _) = ep2.next().await.unwrap();
+    assert_eq!(f1, f2);
+}
+
+/// This test seems to be a bit flaky when run in combination with
+/// other tests and I'm not really sure why...
+///
+/// You can run it on its own with the following cargo invocation:
+///
+/// cargo test long_running -- --ignored
+#[ignore]
+#[async_std::test]
+async fn long_running() {
+    let (ep1, ep2) = setup_eps(7010, 7011).await;
+
+    {
+        let f1 = Frame::dummy();
+        ep1.send(f1.clone(), Target::Single(0)).await.unwrap();
+        let (f2, _) = ep2.next().await.unwrap();
+        assert_eq!(f1, f2);
+    }
+
+    // Wait for a bit I guess...
+    async_std::task::sleep(std::time::Duration::from_secs(10)).await;
+
+    {
+        let f1 = Frame::dummy();
+        ep1.send(f1.clone(), Target::Single(0)).await.unwrap();
+        let (f2, _) = ep2.next().await.unwrap();
+        assert_eq!(f1, f2);
     }
 }
