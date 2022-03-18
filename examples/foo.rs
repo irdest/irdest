f6cb105874b8f3d2a63567b85e6293ef56cb6609
Yureka <yuka@yuka.dev>
2022-03-18 19:23:42 +0100

    proper api using blockstorage

--- examples/foo.rs
+++ examples/foo.rs
@@ -1,12 +1,17 @@
-fn main() {
+use eris::{BlockReference, BlockSize};
+use std::collections::HashMap;
+
+#[async_std::main]
+async fn main() {
     let content = [0; 4096];
     let key = [0; 32];
-    let encoded = eris::encode(&content, key, eris::BlockSize::_1K);
-    println!("root-reference: {}", base32::encode(base32::Alphabet::RFC4648 { padding: false }, &encoded.1));
-    println!("root-key: {}", base32::encode(base32::Alphabet::RFC4648 { padding: false }, &encoded.2));
+    let mut blocks = HashMap::<BlockReference, Vec<u8>>::new();
+    let (root_reference, root_key) = eris::encode(&content, &key, BlockSize::_1K, &mut blocks).await.unwrap();
+    println!("root-reference: {}", base32::encode(base32::Alphabet::RFC4648 { padding: false }, &*root_reference));
+    println!("root-key: {}", base32::encode(base32::Alphabet::RFC4648 { padding: false }, &*root_key));
     println!("blocks:");
-    for block in encoded.0 {
-        println!("{}", base32::encode(base32::Alphabet::RFC4648 { padding: false }, &block));
+    for (reference, block) in blocks {
+        println!("{}: {}", base32::encode(base32::Alphabet::RFC4648 { padding: false }, &*reference), base32::encode(base32::Alphabet::RFC4648 { padding: false }, &block));
     }
     println!("");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ use blake2::{Blake2b, Blake2bMac, Digest, digest::consts::U32, digest::Update, d
 use chacha20::ChaCha20;
 use chacha20::cipher::{KeyIvInit, StreamCipher};
 use thiserror::Error as ThisError;
-use std::collections::HashMap;
 use async_trait::async_trait;
+use std::collections::HashMap;
+use std::ops::Deref;
 
 #[derive(ThisError, Debug)]
 pub enum Error {
@@ -18,11 +19,12 @@ pub enum BlockSize {
     _32K,
 }
 
-impl From<BlockSize> for usize {
-    fn from(block_size: BlockSize) -> usize {
-        match block_size {
-            BlockSize::_1K => 1024,
-            BlockSize::_32K => 32 * 1024,
+impl Deref for BlockSize {
+    type Target = usize;
+    fn deref(&self) -> &usize {
+        match self {
+            BlockSize::_1K => &1024,
+            BlockSize::_32K => &(32 * 1024),
         }
     }
 }
@@ -36,6 +38,13 @@ impl From<[u8; 32]> for BlockReference {
     }
 }
 
+impl Deref for BlockReference {
+    type Target = [u8; 32];
+    fn deref(&self) -> &[u8; 32] {
+        &self.0
+    }
+}
+
 #[derive(Clone)]
 pub struct BlockKey([u8; 32]);
 
@@ -45,103 +54,120 @@ impl From<[u8; 32]> for BlockKey {
     }
 }
 
+impl Deref for BlockKey {
+    type Target = [u8; 32];
+    fn deref(&self) -> &[u8; 32] {
+        &self.0
+    }
+}
+
 pub type RKPair = (BlockReference, BlockKey);
 
 #[async_trait]
 pub trait BlockStorageRead {
-    async fn fetch_block(&self, reference: &BlockReference) -> std::io::Result<Option<Vec<u8>>>;
+    async fn fetch(&self, reference: &BlockReference) -> std::io::Result<Option<Vec<u8>>>;
 }
 
 #[async_trait]
 pub trait BlockStorageWrite {
-    async fn store_block(&mut self, block: &[u8]) -> std::io::Result<()>;
+    async fn store(&mut self, block: &[u8]) -> std::io::Result<()>;
 }
 
 #[async_trait]
 impl BlockStorageRead for HashMap<BlockReference, Vec<u8>> {
-    async fn fetch_block(&self, reference: &BlockReference) -> std::io::Result<Option<Vec<u8>>> {
+    async fn fetch(&self, reference: &BlockReference) -> std::io::Result<Option<Vec<u8>>> {
         Ok(self.get(reference).cloned())
     }
 }
 
 #[async_trait]
 impl BlockStorageWrite for HashMap<BlockReference, Vec<u8>> {
-    async fn store_block(&mut self, block: &[u8]) -> std::io::Result<()> {
+    async fn store(&mut self, block: &[u8]) -> std::io::Result<()> {
         self.insert(block_reference(block), block.to_vec());
         Ok(())
     }
 }
 
-pub fn encode(content: &[u8], convergence_secret: &[u8; 32], block_size: BlockSize) -> (Vec<Vec<u8>>, RKPair) {
-    let mut level = 0;
-
-    let (mut blocks, mut rk_pairs) = split_content(content, convergence_secret, block_size);
-
-    while rk_pairs.len() > 1 {
-        let (mut level_blocks, new_rk_pairs) = collect_rk_pairs(rk_pairs, convergence_secret, block_size);
-        rk_pairs = new_rk_pairs;
-
-        blocks.append(&mut level_blocks);
-        level += 1;
-    }
-
-    let root = rk_pairs[0].clone();
-
-    (blocks, root)
+pub struct Encoder<'a, S: BlockStorageWrite> {
+    convergence_secret: [u8; 32],
+    block_size: BlockSize,
+    block_storage: &'a mut S,
 }
 
-fn split_content(content: &[u8], convergence_secret: &[u8; 32], block_size: BlockSize) -> (Vec<Vec<u8>>, Vec<RKPair>) {
-    let mut blocks = vec![];
-    let mut rk_pairs = vec![];
-
-    let padded = {
-        let mut buffer = content.to_vec();
-        pad(&mut buffer, block_size);
-        buffer
+pub async fn encode<S: BlockStorageWrite>(content: &[u8], convergence_secret: &[u8; 32], block_size: BlockSize, block_storage: &mut S) -> std::io::Result<RKPair> {
+    let mut encoder = Encoder {
+        convergence_secret: convergence_secret.clone(),
+        block_size,
+        block_storage,
     };
-
-    for content_block in padded.chunks_exact(block_size.into()) {
-        let (encrypted_block, rk_pair) = encrypt_block(content_block, convergence_secret);
-        blocks.push(encrypted_block);
-        rk_pairs.push(rk_pair);
-    }
-
-    (blocks, rk_pairs)
+    encoder.encode(content).await
 }
 
-fn collect_rk_pairs(mut input_rk_pairs: Vec<RKPair>, convergence_secret: &[u8; 32], block_size: BlockSize) -> (Vec<Vec<u8>>, Vec<RKPair>) {
-    let arity = usize::from(block_size) / 64;
+impl<'a, S: BlockStorageWrite> Encoder<'a, S> {
+    pub async fn encode(&mut self, content: &[u8]) -> std::io::Result<RKPair> {
+        let mut level = 0;
+        let mut rk_pairs = self.split_content(content).await?;
 
-    let mut blocks = vec![];
-    let mut output_rk_pairs = vec![];
+        while rk_pairs.len() > 1 {
+            let new_rk_pairs = self.collect_rk_pairs(rk_pairs).await?;
+            rk_pairs = new_rk_pairs;
+            level += 1;
+        }
 
-    while input_rk_pairs.len() % arity != 0 {
-        input_rk_pairs.push(([0; 32].into(), [0; 32].into()));
+        let root = rk_pairs.remove(0);
+        Ok(root)
     }
 
-    for rk_pairs_for_node in input_rk_pairs.chunks_exact(arity) {
-        let node = {
-            let mut buffer = vec![];
-            for pair in rk_pairs_for_node {
-                buffer.extend_from_slice(&pair.0.0);
-                buffer.extend_from_slice(&pair.1.0);
-            }
+    async fn split_content(&mut self, content: &[u8]) -> std::io::Result<Vec<RKPair>> {
+        let mut rk_pairs = vec![];
+
+        let padded = {
+            let mut buffer = content.to_vec();
+            pad(&mut buffer, self.block_size);
             buffer
         };
 
-        let (block, rk_pair) = encrypt_block(&node, convergence_secret);
+        for content_block in padded.chunks_exact(*self.block_size) {
+            let (encrypted_block, rk_pair) = encrypt_block(content_block, &self.convergence_secret);
+            self.block_storage.store(&encrypted_block).await?;
+            rk_pairs.push(rk_pair);
+        }
 
-        blocks.push(block);
-        output_rk_pairs.push(rk_pair);
+        Ok(rk_pairs)
     }
 
-    (blocks, output_rk_pairs)
+    async fn collect_rk_pairs(&mut self, mut input_rk_pairs: Vec<RKPair>) -> std::io::Result<Vec<RKPair>> {
+        let arity = *self.block_size / 64;
+
+        let mut output_rk_pairs = vec![];
+
+        while input_rk_pairs.len() % arity != 0 {
+            input_rk_pairs.push(([0; 32].into(), [0; 32].into()));
+        }
+
+        for rk_pairs_for_node in input_rk_pairs.chunks_exact(arity) {
+            let node = {
+                let mut buffer = vec![];
+                for pair in rk_pairs_for_node {
+                    buffer.extend_from_slice(&pair.0.0);
+                    buffer.extend_from_slice(&pair.1.0);
+                }
+                buffer
+            };
+
+            let (block, rk_pair) = encrypt_block(&node, &self.convergence_secret);
+
+            self.block_storage.store(&block).await?;
+            output_rk_pairs.push(rk_pair);
+        }
+
+        Ok(output_rk_pairs)
+    }
 }
 
 fn pad(input: &mut Vec<u8>, block_size: BlockSize) {
-    let block_size: usize = block_size.into();
     input.push(0x80);
-    while input.len() % block_size != 0 {
+    while input.len() % *block_size != 0 {
         input.push(0x0);
     }
 }
@@ -154,7 +180,7 @@ fn unpad(input: &mut Vec<u8>, block_size: BlockSize) -> Result {
             Some(0x80) => return Ok(()),
             _ => return Err(Error::Padding),
         }
-        if old_len - input.len() > usize::from(block_size) {
+        if old_len - input.len() > *block_size {
             return Err(Error::Padding);
         }
     }
