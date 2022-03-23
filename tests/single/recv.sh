68d3269244d6a44010a4ba6475abac43a51d6e25
Katharina Fey <kookie@spacekookie.de>
2022-03-24 00:51:22 +0100

    ci: add simple integration test ci job

--- tests/single/recv.sh
+++ tests/single/recv.sh
@@ -13,3 +13,7 @@ env XDG_CONFIG_HOME=state/single-node/recv ../target/debug/ratcat --register
 cat state/single-node/recv/config | jq .addr | tr -d '"' | tee > state/single-node/recv-addr
 
 env XDG_CONFIG_HOME=state/single-node/recv ../target/debug/ratcat --recv --count 1
+
+# After the message has been received we shut down ratmand
+# todo hide this behind a flag
+pkill ratmand
