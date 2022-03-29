e9bd91ffd306245924c64792b1d95829e7e59441
embr <git@liclac.eu>
2022-03-29 18:29:16 +0200

    ratman/webui: use `yarn serve --proxy`

--- ratman/webui/app/adapters/application.js
+++ ratman/webui/app/adapters/application.js
@@ -1,7 +1,5 @@
 import RESTAdapter from '@ember-data/adapter/rest';
-import ENV from 'webui/config/environment';
 
 export default class ApplicationAdapter extends RESTAdapter {
   namespace = 'v1';
-  host = ENV.apiHost;
 }
