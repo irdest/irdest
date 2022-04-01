0a79242a8dc94ab0ccdc4cf9eb9664f4cd2c5e01
embr <git@liclac.eu>
2022-04-01 12:34:49 +0200

    nix/ratman: work around webui cross-compilation issues
    There's an upstream nixpkgs issue, which breaks eg. `pkgsStatic.yarn2nix`:
    https://github.com/NixOS/nixpkgs/issues/116207
    
    Thankfully, the output of `ratman-webui` is a pile of arch-independent,
    static assets, so we don't actually need this to work; we only need the
    build scripts to run on the builder.
    
    --------------------------------------------------------------------------
    
    > $ nix-shell -p pkgsStatic.yarn2nix
    > error: attribute 'runtimeShell' missing
    >
    >        at /home/embr/src/nixpkgs/pkgs/top-level/all-packages.nix:731:17:
    >
    >           730|       substitutions = {
    >           731|         shell = targetPackages.runtimeShell;
    >              |                 ^
    >           732|         passthru.tests = tests.makeWrapper;
    > (use '--show-trace' to show detailed location information)

--- nix/ratman/default.nix
+++ nix/ratman/default.nix
@@ -1,11 +1,17 @@
 { lib
-, callPackage
 , rustPlatform
 , protobuf
 , libsodium
 , pkg-config
-, ratman-webui
 }:
+
+let
+  # Build the webui assets natively on the builder, even when cross-compiling.
+  #
+  # This works around an issue where eg. `pkgsStatic.yarn2nix` fails to evaluate.
+  # See: https://github.com/NixOS/nixpkgs/issues/116207
+  ratman-webui = (import <nixpkgs> { }).callPackage ../ratman-webui { };
+in
 
 rustPlatform.buildRustPackage rec {
   pname = "ratman";
