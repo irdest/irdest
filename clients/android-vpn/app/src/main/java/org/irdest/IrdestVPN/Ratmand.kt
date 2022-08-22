81bb015367df093f681641948ca438649bc16afa
oooh_chew <gx_339-4@pm.me>
2022-08-22 20:10:11 +0200

    clients/android-vpn: handle thread exception

--- clients/android-vpn/app/src/main/java/org/irdest/IrdestVPN/Ratmand.kt
+++ clients/android-vpn/app/src/main/java/org/irdest/IrdestVPN/Ratmand.kt
@@ -1,38 +1,41 @@
 package org.irdest.IrdestVPN
 
 import android.util.Log
+import java.lang.RuntimeException
 
 class Ratmand {
     private val TAG = Ratmand::class.java.simpleName
-
     // JNI constructs the name of function that it will call is
     // Java_<domain>_<class>_<methodname> (Java_org_irdest_ratman_Ratmand_ratrun).
     private external fun ratrun(test_str: String): String
 
-    var arive = true
+    val handler = object : Thread.UncaughtExceptionHandler {
+        override fun uncaughtException(t: Thread, e: Throwable) {
+            Log.e(TAG, "uncaughtException: ratmand thread throw exception", e)
+        }
+    }
 
     fun runRatmand(test_string: String?) {
-        Log.d(TAG, "runRatmand: runRatmand is called")
-
-        arive = true
-
         val ratmandThread = object : Thread() {
             override fun run() {
-                Log.d(TAG, "run_ratmand(Ratmand): Current thread = "
-                        + Thread.currentThread())
-
-                ratrun("test")
-
+                try {
+                    ratrun("test")
+                } catch(e: InterruptedException){
+                    Log.e(TAG, "run: ratmand thread interrupted ", e)
+                }
+                // Catch uncaught exception
                 Log.d(TAG, "run: ratmand is stopped")
+                throw RuntimeException();
             }
         }
-        ratmandThread.isDaemon = true
         ratmandThread.stackTrace
+        ratmandThread.isDaemon = true
+        ratmandThread.setUncaughtExceptionHandler(handler)
+
         ratmandThread.start()
     }
 
     fun stopRatmand() {
-        Log.d(TAG, "stopRatmand: Stop Ratmand is called")
-        arive = false
+        //TODO: Stop ratmand
     }
 }
