c34ced159d7192f67bf00e4fc8d98b757989250f
SEGFAULT <fakespamtrap@gmail.com>
2022-09-21 20:48:04 +0200

    LoraNetmod: Implement bare minium frame serialisation

--- netmods/netmod-lora/src/lib.rs
+++ netmods/netmod-lora/src/lib.rs
@@ -4,6 +4,7 @@
 extern crate tracing;
 
 use netmod::{Endpoint, Frame, Result, Target};
+use irdest_firmware_util::{decode_frame, encode_frame};
 
 use async_std::{channel, sync::Arc, sync::Mutex, task};
 use async_trait::async_trait;
@@ -18,28 +19,28 @@ const RADIO_MTU: usize = 255;
 const BUFFER_SIZE: usize = 32;
 const IRDEST_MAGIC: u8 = 0xCA;
 
-#[repr(u8)]
-#[derive(Copy, Clone, Serialize, Deserialize)]
-enum CtrlTypeCode {
-    Invalid = 0x00,
-    Data,
-    RtxReq,
-}
+// #[repr(u8)]
+// #[derive(Copy, Clone, Serialize, Deserialize)]
+// enum CtrlTypeCode {
+//     Invalid = 0x00,
+//     Data,
+//     RtxReq,
+// }
 
-#[repr(packed)]
-#[derive(Copy, Clone, Serialize, Deserialize)]
-struct CtrlHeader {
-    magic: u8,
-    packet_type: CtrlTypeCode,
-    length: u8,
-}
+// #[repr(packed)]
+// #[derive(Copy, Clone, Serialize, Deserialize)]
+// struct CtrlHeader {
+//     magic: u8,
+//     packet_type: CtrlTypeCode,
+//     length: u8,
+// }
 
-#[repr(packed)]
-#[derive(Serialize, Deserialize)]
-struct LoraPacket<'a> {
-    header: CtrlHeader,
-    payload: &'a [u8],
-}
+// #[repr(packed)]
+// #[derive(Serialize, Deserialize)]
+// struct LoraPacket<'a> {
+//     header: CtrlHeader,
+//     payload: &'a [u8],
+// }
 
 pub struct LoraEndpoint {
     rx: channel::Receiver<Frame>,
@@ -68,9 +69,8 @@ impl LoraEndpoint {
         let mut buffer: [u8; RADIO_MTU] = [0; RADIO_MTU];
         loop {
             self.serial.lock().await.read_exact(&mut buffer).unwrap();
-            todo!("deserialise frame");
-            let frame;
-            c.send(frame).await;
+            let frame = decode_frame(&mut buffer.as_slice()).unwrap();
+            c.send(frame).await.unwrap();
         }
     }
 }
@@ -78,22 +78,24 @@ impl LoraEndpoint {
 #[async_trait]
 impl Endpoint for LoraEndpoint {
     fn size_hint(&self) -> usize {
-        RADIO_MTU - std::mem::size_of::<CtrlHeader>()
+        RADIO_MTU // - std::mem::size_of::<CtrlHeader>()
     }
 
-    async fn send(&self, frame: Frame, target: Target, exclude: Option<u16>) -> Result<()> {
+    async fn send(&self, frame: Frame, _target: Target, exclude: Option<u16>) -> Result<()> {
         if exclude.is_some() {
             return Ok(());
         }
 
-        let ctrl = CtrlHeader {
-            magic: IRDEST_MAGIC,
-            packet_type: CtrlTypeCode::Data,
-            length: self.size_hint() as u8,
-        };
+        // let ctrl = CtrlHeader {
+        //     magic: IRDEST_MAGIC,
+        //     packet_type: CtrlTypeCode::Data,
+        //     length: self.size_hint() as u8,
+        // };
 
-        todo!("serialise Frame");
-        let buffer: [u8; RADIO_MTU] = [0; RADIO_MTU];
+        let mut buffer = vec![];
+        encode_frame(&mut buffer, &frame).unwrap();
+
+        assert_eq!(buffer.len(), RADIO_MTU);
 
         self.serial.lock().await.write(&buffer).unwrap();
 
