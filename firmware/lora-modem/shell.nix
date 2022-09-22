eb7a27bff74cf409171cb8aa8580e7d255e299c6
Katharina Fey <kookie@spacekookie.de>
2022-09-23 01:45:19 +0200

    firmware: add initial nix cross-build environment

--- firmware/lora-modem/shell.nix
+++ firmware/lora-modem/shell.nix
@@ -8,12 +8,27 @@
 
 with import <nixpkgs> {
   crossSystem = (import <nixpkgs/lib>).systems.examples.armhf-embedded // {
-    rustc.config = "thumbv7em-none-eabi";
+    rustc.config = "thumbv7em-none-eabihf";
   };
+  overlays = [
+    (self: super: {
+      rust_1_62 = super.rust_1_62 // {
+        packages = super.rust_1_62.packages // {
+          stable = super.rust_1_62.packages.stable.overrideScope' (self: super: {
+            rustc = super.rustc.overrideAttrs ({ configureFlags ? [], ... }: {
+              configureFlags = configureFlags ++ [ "--disable-docs" ];
+            });
+          });
+        };
+      };
+    })
+  ];
 };
 stdenv.mkDerivation {
   name = "irdest-firmware";
   nativeBuildInputs = with pkgsBuildHost; [
-    rustc cargo rustfmt rust-analyzer clangStdenv
+    rustc cargo cargo-binutils lld clangStdenv
+
+    gdb openocd
   ];
 }
