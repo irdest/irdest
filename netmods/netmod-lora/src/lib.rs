85abb90229a3e7ad606736ff73a8dc67ebdcc5f6
SEGFAULT <fakespamtrap@gmail.com>
2022-09-22 21:17:14 +0200

    LoraModem: implement lora link layer packet format.

--- netmods/netmod-lora/src/lib.rs
+++ netmods/netmod-lora/src/lib.rs
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later WITH LicenseRef-AppStore
 
-#[macro_use]
-extern crate tracing;
-
-use netmod::{Endpoint, Frame, Result, Target};
+use netmod::{Endpoint, Frame, Target};
+use netmod::Result as NMResult;
 use irdest_firmware_util::{decode_frame, encode_frame};
 
 use async_std::{channel, sync::Arc, sync::Mutex, task};
 use async_trait::async_trait;
-
-use serde::{Deserialize, Serialize};
 
 use serialport::TTYPort;
 use std::io::prelude::*;
@@ -19,28 +15,65 @@ const RADIO_MTU: usize = 255;
 const BUFFER_SIZE: usize = 32;
 const IRDEST_MAGIC: u8 = 0xCA;
 
-// #[repr(u8)]
-// #[derive(Copy, Clone, Serialize, Deserialize)]
-// enum CtrlTypeCode {
-//     Invalid = 0x00,
-//     Data,
-//     RtxReq,
-// }
+#[repr(u8)]
+#[derive(Copy, Clone, Debug)]
+enum CtrlTypeCode {
+    _Invalid = 0x00,
+    Data,
+    RtxReq,
+}
 
-// #[repr(packed)]
-// #[derive(Copy, Clone, Serialize, Deserialize)]
-// struct CtrlHeader {
-//     magic: u8,
-//     packet_type: CtrlTypeCode,
-//     length: u8,
-// }
+#[repr(packed)]
+#[derive(Debug)]
+struct CtrlHeader {
+    magic: u8,
+    packet_type: CtrlTypeCode,
+    length: u8,
+}
 
-// #[repr(packed)]
-// #[derive(Serialize, Deserialize)]
-// struct LoraPacket<'a> {
-//     header: CtrlHeader,
-//     payload: &'a [u8],
-// }
+const PAYLOAD_SIZE: usize = RADIO_MTU - std::mem::size_of::<CtrlHeader>();
+
+#[derive(Debug)]
+enum LoraPacketError {
+    ControlCodeError(u8),
+}
+
+#[repr(packed)]
+#[derive(Debug)]
+struct LoraPacket {
+    header: CtrlHeader,
+    payload: [u8; PAYLOAD_SIZE],
+}
+
+impl LoraPacket {
+    
+    // HERE BE DRAGONS: ENDIANESS NOT HANDLED!
+    fn encode(&self) -> [u8; RADIO_MTU] {
+        let mut out = [0; RADIO_MTU];
+        out[0] = self.header.magic;
+        out[1] = self.header.packet_type as u8;
+        out[2] = self.header.length;
+        // todo mut slice writeall
+        for i in 0 .. PAYLOAD_SIZE {
+            out[i+3] = self.payload[i];
+        }
+        out
+    }
+
+    fn decode(data: [u8; RADIO_MTU]) -> Result<Self, LoraPacketError> {
+        let magic = data[0];
+        let length = data[2];
+        let packet_type = match data[1] {
+            1 => Ok(CtrlTypeCode::Data),
+            2 => Ok(CtrlTypeCode::RtxReq),
+            _ => Err(LoraPacketError::ControlCodeError(data[1])),
+        }?;
+
+        let header = CtrlHeader { magic, packet_type, length };
+        
+        Ok(Self{header, payload: data[3..].try_into().unwrap()})
+    }
+}
 
 pub struct LoraEndpoint {
     rx: channel::Receiver<Frame>,
@@ -66,9 +99,17 @@ impl LoraEndpoint {
     }
 
     async fn read_serial(self: Arc<Self>, c: channel::Sender<Frame>) {
-        let mut buffer: [u8; RADIO_MTU] = [0; RADIO_MTU];
+        let buffer: [u8; RADIO_MTU] = [0; RADIO_MTU];
         loop {
-            self.serial.lock().await.read_exact(&mut buffer).unwrap();
+            let mut rx_packet = LoraPacket::decode(buffer).unwrap();
+
+            // check header format is correct.
+            if rx_packet.header.magic != IRDEST_MAGIC {
+                continue;
+            }
+            
+
+            self.serial.lock().await.read_exact(&mut rx_packet.payload).unwrap();
             let frame = decode_frame(&mut buffer.as_slice()).unwrap();
             c.send(frame).await.unwrap();
         }
@@ -78,31 +119,37 @@ impl LoraEndpoint {
 #[async_trait]
 impl Endpoint for LoraEndpoint {
     fn size_hint(&self) -> usize {
-        RADIO_MTU // - std::mem::size_of::<CtrlHeader>()
+        PAYLOAD_SIZE
     }
 
-    async fn send(&self, frame: Frame, _target: Target, exclude: Option<u16>) -> Result<()> {
+    async fn send(&self, frame: Frame, _target: Target, exclude: Option<u16>) -> NMResult<()> {
         if exclude.is_some() {
             return Ok(());
         }
 
-        // let ctrl = CtrlHeader {
-        //     magic: IRDEST_MAGIC,
-        //     packet_type: CtrlTypeCode::Data,
-        //     length: self.size_hint() as u8,
-        // };
+        let header = CtrlHeader {
+            magic: IRDEST_MAGIC,
+            packet_type: CtrlTypeCode::Data,
+            length: self.size_hint() as u8,
+        };
 
-        let mut buffer = vec![];
-        encode_frame(&mut buffer, &frame).unwrap();
+        let mut payload = vec![];
+        encode_frame(&mut payload, &frame).unwrap();
 
-        assert_eq!(buffer.len(), RADIO_MTU);
+        assert_eq!(payload.len(), PAYLOAD_SIZE);
+
+        let payload = payload.as_slice().try_into().unwrap();
+
+        let tx_packet = LoraPacket { header, payload };
+
+        let buffer = tx_packet.encode();
 
         self.serial.lock().await.write(&buffer).unwrap();
 
         Ok(())
     }
 
-    async fn next(&self) -> Result<(Frame, Target)> {
+    async fn next(&self) -> NMResult<(Frame, Target)> {
         let frame = self.rx.recv().await.unwrap();
         Ok((frame, Target::Single(0)))
     }
