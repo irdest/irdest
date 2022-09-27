4d3b02fc666740bec3cfe79c965c3cabc1546e06
Katharina Fey <kookie@spacekookie.de>
2022-09-27 20:24:38 +0200

    client/mblog: Add sample topic footer

--- clients/irdest-mblog/src/bin/irdest-mblog-gtk/topic.rs
+++ clients/irdest-mblog/src/bin/irdest-mblog-gtk/topic.rs
@@ -1,4 +1,8 @@
-use gtk::{Box as GtkBox, Button, Label as GtkLabel, Orientation, Stack, StackSidebar};
+use gtk::prelude::*;
+use gtk::{
+    builders::BoxBuilder, Align, Box as GtkBox, Button, Entry, Label as GtkLabel, Orientation,
+    Stack, StackSidebar,
+};
 
 /// Topics UI management container
 pub struct Topics {
@@ -11,6 +15,7 @@ impl Topics {
         let stack = Stack::new();
         let sidebar = StackSidebar::new();
         sidebar.set_stack(&stack);
+        stack.set_vhomogeneous(true);
 
         Self { stack, sidebar }
     }
@@ -22,12 +27,48 @@ impl Topics {
 
 pub struct Topic {
     inner: GtkBox,
+    footer: TopicFooter,
 }
 
 impl Topic {
     pub fn new(initialise: impl Fn(&GtkBox)) -> Self {
-        let inner = GtkBox::new(Orientation::Vertical, 0);
+        let footer = TopicFooter::new();
+        let inner = BoxBuilder::new()
+            .orientation(Orientation::Vertical)
+            .spacing(8)
+            .hexpand(true)
+            .vexpand(true)
+            .margin_start(32)
+            .margin_end(32)
+            .halign(Align::Fill)
+            .valign(Align::Fill)
+            .build();
+
         initialise(&inner);
-        Self { inner }
+        inner.append(&footer.inner);
+        Self { inner, footer }
+    }
+
+    pub fn add_message(&self, msg: &str) {
+        // TODO: create a frame, put the message in there, then add to
+        // the inner box
+    }
+}
+
+/// Display a footer at the bottom of the topic screen
+///
+/// Renders
+pub struct TopicFooter {
+    inner: GtkBox,
+    entry: Entry,
+}
+
+impl TopicFooter {
+    pub fn new() -> Self {
+        let inner = GtkBox::new(Orientation::Horizontal, 0);
+        let entry = Entry::new();
+        inner.append(&entry);
+
+        Self { inner, entry }
     }
 }
