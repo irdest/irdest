d78203544fb71712be862838decfe4a1af6bd4a1
Katharina Fey <kookie@spacekookie.de>
2023-09-19 23:49:39 +0200

    libratman: clean up files

--- ratman/libratman/src/types/frames/mod.rs
+++ ratman/libratman/src/types/frames/mod.rs
@@ -8,11 +8,16 @@ mod parse;
 
 ////// Frame type exports
 pub use announce::*;
-pub use carrier::{CarrierFrame, CarrierFrameMeta, CarrierFrameV1};
+pub use carrier::*;
+pub use manifest::*;
 use rand::RngCore;
 
 pub(self) fn random_payload(size: usize) -> Vec<u8> {
     let mut buf = vec![0; size];
     rand::thread_rng().fill_bytes(&mut buf);
     buf.into()
+}
+
+pub fn data_frame(data: Vec<u8>) -> CarrierFrame {
+    todo!()
 }
