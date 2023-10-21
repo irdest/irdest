26f82540626bdc041c6a0aefc665af4627fc1898
Katharina Fey <kookie@spacekookie.de>
2023-10-21 12:22:04 +0200

    async-eris: add a test for a larger message

---
+++ utils/eris-rs/examples/big_message.rs
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: 2023 Katharina Fey <kookie@spacekookie.de>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later WITH LicenseRef-AppStore
+
+use async_eris as eris;
+use eris::{BlockSize, MemoryStorage};
+use rand::{rngs::OsRng, RngCore};
+
+#[async_std::main]
+async fn main() {
+    let mut content = vec![0; 1024 * 32];
+    OsRng {}.fill_bytes(&mut content);
+
+    println!("Taking {} bytes of content...", content.len());
+
+    let mut blocks = MemoryStorage::new();
+    // Randomly chosen key :)
+    let key = [
+        93, 72, 136, 16, 3, 194, 107, 102, 20, 11, 42, 105, 193, 208, 47, 23, 135, 76, 154, 63, 41,
+        84, 85, 108, 86, 0, 90, 58, 6, 112, 22, 4,
+    ];
+
+    let read_capability = eris::encode(&mut &*content, &key, BlockSize::_1K, &mut blocks)
+        .await
+        .unwrap();
+
+    println!("{}", read_capability.urn());
+    println!("{:?}", read_capability);
+    // for (reference, block) in &blocks {
+    //     println!(
+    //         "{}: {}",
+    //         base32::encode(base32::Alphabet::RFC4648 { padding: false }, &**reference),
+    //         base32::encode(base32::Alphabet::RFC4648 { padding: false }, &block)
+    //     );
+    // }
+
+    let mut decoded = vec![];
+    eris::decode(&mut decoded, &read_capability, &blocks)
+        .await
+        .unwrap();
+
+    assert_eq!(decoded, content);
+    println!("Input == Output");
+}
--- utils/eris-rs/src/tests.rs
+++ utils/eris-rs/src/tests.rs
@@ -184,3 +184,44 @@ async fn run_vectors() {
         rx.recv().await.unwrap();
     }
 }
+
+#[async_std::test]
+async fn big_message() {
+    use crate as eris;
+    use eris::{BlockSize, MemoryStorage};
+    use rand::{rngs::OsRng, RngCore};
+
+    let mut content = vec![0; 1024 * 64];
+    OsRng {}.fill_bytes(&mut content);
+
+    println!("Taking {} bytes of content...", content.len());
+
+    let mut blocks = MemoryStorage::new();
+    // Randomly chosen key :)
+    let key = [
+        93, 72, 136, 16, 3, 194, 107, 102, 20, 11, 42, 105, 193, 208, 47, 23, 135, 76, 154, 63, 41,
+        84, 85, 108, 86, 0, 90, 58, 6, 112, 22, 4,
+    ];
+
+    let read_capability = eris::encode(&mut &*content, &key, BlockSize::_1K, &mut blocks)
+        .await
+        .unwrap();
+
+    println!("{}", read_capability.urn());
+    println!("{:?}", read_capability);
+    // for (reference, block) in &blocks {
+    //     println!(
+    //         "{}: {}",
+    //         base32::encode(base32::Alphabet::RFC4648 { padding: false }, &**reference),
+    //         base32::encode(base32::Alphabet::RFC4648 { padding: false }, &block)
+    //     );
+    // }
+
+    let mut decoded = vec![];
+    eris::decode(&mut decoded, &read_capability, &blocks)
+        .await
+        .unwrap();
+
+    assert_eq!(decoded, content);
+    println!("Input == Output");
+}
