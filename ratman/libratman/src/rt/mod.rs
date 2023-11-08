322c000279addacd573f890b360fb1220f2dc21d
Katharina Fey <kookie@spacekookie.de>
2023-11-08 19:08:15 +0100

    libratman: implement basic async system based on tokio

---
+++ ratman/libratman/src/rt/mod.rs
@@ -0,0 +1,63 @@
+//! Shared sync & async runtime utilities
+
+use crate::Result;
+use std::{
+    future::Future,
+    sync::{
+        mpsc::{sync_channel, Receiver, SyncSender},
+        Arc,
+    },
+};
+use tokio::{
+    runtime::{Builder, Runtime},
+    task::LocalSet,
+};
+
+/// Encapsulates a single threaded async system
+pub struct AsyncSystem {
+    label: String,
+    rt: Runtime,
+    set: LocalSet,
+    irq: (SyncSender<()>, Receiver<()>),
+}
+
+impl AsyncSystem {
+    fn new(label: String, stack_mb: usize) -> Arc<Self> {
+        Arc::new(Self {
+            rt: Builder::new_current_thread()
+                .thread_name(&label)
+                .thread_stack_size(1024 * 1024 /* MibiByte */ * stack_mb)
+                .build()
+                .expect("failed to start async thread!"),
+            set: LocalSet::new(),
+            label,
+            irq: sync_channel(4),
+        })
+    }
+
+    pub fn async_interrupt(self: &Arc<Self>) {
+        self.irq.0.send(());
+    }
+
+    fn root_exec(&self, f: impl Future<Output = Result<()>> + Send + 'static) -> Result<()> {
+        let _g = self.rt.handle().enter();
+        let ok = self.rt.handle().block_on(f);
+        drop(_g);
+        ok
+    }
+}
+
+/// Spawn new worker thread with an async system launcher
+pub fn new_async_thread(
+    label: String,
+    stack_mb: usize,
+    f: impl Future<Output = Result<()>> + Send + 'static,
+) {
+    std::thread::spawn(move || {
+        let system = AsyncSystem::new(label, stack_mb);
+        match system.root_exec(f) {
+            Ok(_) => info!("Worker thread {} completed successfully!", system.label),
+            Err(e) => error!("Worker thread {} encountered an error: {}", system.label, e),
+        }
+    });
+}
