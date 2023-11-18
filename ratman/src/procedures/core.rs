3aac4de0652b1405d2dab260e138ab1b1cb70ab2
Katharina Fey <kookie@spacekookie.de>
2023-11-18 14:30:42 +0100

    ratmand: fix tests

--- ratman/src/procedures/core.rs
+++ ratman/src/procedures/core.rs
@@ -2,7 +2,10 @@ use crate::{
     core::{run_message_assembler, Journal, LinksMap, RouteTable},
     dispatch::BlockCollector,
 };
-use libratman::tokio::{sync::mpsc, task::spawn_local};
+use libratman::{
+    tokio::{sync::mpsc, task::spawn_local},
+    types::Letterhead,
+};
 use std::sync::Arc;
 
 /// The core is a set of modules that densely depend on each other
@@ -16,22 +19,31 @@ pub(crate) struct Core {
     pub(crate) routes: Arc<RouteTable>,
 }
 
-pub(crate) async fn start_core() -> Core {
+pub(crate) async fn start_core() -> (Core, mpsc::Receiver<Letterhead>) {
     let drivers = LinksMap::new();
     let routes = RouteTable::new();
-    let journal = Journal::new();
+    let (journal, m_notify_r) = Journal::new();
 
     let (jtx, jrx) = mpsc::channel(16);
+    let (lh_notify_t, lh_notify_r) = mpsc::channel(16);
+
     let collector = BlockCollector::new(jtx);
 
     // Dispatch the runners
     spawn_local(Arc::clone(&journal).run_block_acceptor(jrx));
-    spawn_local(run_message_assembler(Arc::clone(&journal)));
+    spawn_local(run_message_assembler(
+        Arc::clone(&journal),
+        m_notify_r,
+        lh_notify_t,
+    ));
 
-    Core {
-        collector,
-        drivers,
-        journal,
-        routes,
-    }
+    (
+        Core {
+            collector,
+            drivers,
+            journal,
+            routes,
+        },
+        lh_notify_r,
+    )
 }
