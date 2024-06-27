2e80a2e8d7fcecf4b1d08f4e020042abb6bd69a9
Katharina Fey <kookie@spacekookie.de>
2024-06-27 18:37:56 +0200

    Allow chunk size to be indicated as kb or mb

--- clients/ratman-tools/src/send.rs
+++ clients/ratman-tools/src/send.rs
@@ -42,7 +42,37 @@ pub async fn send(ipc: &Arc<RatmanIpc>, base_args: BaseArgs, matches: &ArgMatche
     let (addr, auth) = base_args.identity_data?;
     let to_addr = parse_field::<String>(matches, "to-address");
     let to_space = parse_field::<String>(matches, "to-space");
-    let chunk_size = *parse_field::<u64>(matches, "chunk-size").unwrap_or(&0);
+    let chunk_size_str: String = parse_field::<String>(matches, "chunk-size")
+        .map(|x| x.to_lowercase())
+        .unwrap_or("0".to_string());
+
+    let chunk_size: u64 = if chunk_size_str.contains("k") {
+        chunk_size_str
+            .replace("k", "")
+            .parse::<u64>()
+            .map_err(|_| {
+                UserError::InvalidInput(
+                    chunk_size_str.replace("k", "").to_string(),
+                    Some(format!("valid integer")),
+                )
+            })?
+            * /* Kibibytes */ 1024
+    } else if chunk_size_str.contains("m") {
+        chunk_size_str
+            .replace("m", "")
+            .parse::<u64>()
+            .map_err(|_| {
+                UserError::InvalidInput(
+                    chunk_size_str.replace("m", "").to_string(),
+                    Some(format!("valid integer")),
+                )
+            })?
+            * /* Mibibytes */ (1024 * 1024)
+    } else {
+        chunk_size_str.parse().map_err(|_| {
+            UserError::InvalidInput(chunk_size_str.to_string(), Some(format!("valid integer")))
+        })?
+    };
 
     eprintln!("Generate letterheads");
     let to = match (to_addr, to_space) {
@@ -77,9 +107,7 @@ pub async fn send(ipc: &Arc<RatmanIpc>, base_args: BaseArgs, matches: &ArgMatche
         loop {
             let mut stdin = tokio::io::stdin().take(chunk_size);
             eprintln!("Send {chunk_size} sized stream chunk...");
-            if ipc.send_many(auth, to.clone(), &mut stdin).await.is_err() {
-                break;
-            }
+            ipc.send_many(auth, to.clone(), &mut stdin).await?;
         }
     }
 
